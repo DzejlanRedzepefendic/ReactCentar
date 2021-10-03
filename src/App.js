4152803490f1df81b64c842ba8e831dede28b88f
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-10-03 19:59:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h2 className='text'>
+        Slow-carb next level shoindxgoitch ethical authentic, scenester sriacha
+      </h2>
+      <button className='btn'>Button</button>
+    </div>
+  )
+}
+
+export default App
