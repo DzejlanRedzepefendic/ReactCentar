6e8743262cbc76d8d12f597119c51b4eea64fbf4
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-10-04 20:43:23 +0200

    components/props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Welcome from './Welcome'
 function App() {
+  const names = ['Suad', 'Blaa', 'Arijan', 'Truc']
   return (
-    <div>
-      <h2 className='text'>
-        Slow-carb next level shoindxgoitch ethical authentic, scenester sriacha
-      </h2>
-      <button className='btn'>Button</button>
+    <div className='App'>
+      {names.map((value, index) => {
+        return <Welcome name={value} key={index} />
+      })}
     </div>
   )
 }
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome({ name = 'Unknown' }) {
+  return <h1>Welcome {name}</h1>
+}
+
+export default Welcome
